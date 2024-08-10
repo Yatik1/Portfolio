00fd6aa2e3f9ece76c5b9862643e9e02c66b1510
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-10 23:11:03 +0530

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Header from "@/components/ui/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Header 
+          title="Yatik Srivastava"
+          textColor="text-[#d4d3d3] hover:text-stone-400"
+        />
         {children}
       </body>
     </html>
