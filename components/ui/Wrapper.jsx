b2db82284eaf45590ff1a174426e7ecee9b561d2
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-10 23:11:57 +0530

    Wrapper

--- components/ui/Wrapper.jsx
+++ components/ui/Wrapper.jsx
@@ -2,7 +2,7 @@
 
 function Wrapper({children , styles}) {
     return (
-        <div className={`${styles} bg-[#F7F7F7] flex items-center justify-center`}>
+        <div className={`${styles} bg-[#F7F7F7] flex flex-col items-center justify-center gap-5 rounded-xl`}>
             {children}
         </div>
     )
