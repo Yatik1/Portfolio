07216255bdb5912d50f90268b112ec853fbae652
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-12 23:48:02 +0530

    wrapper component forward ref

--- components/ui/Wrapper.jsx
+++ components/ui/Wrapper.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 
-
-function Wrapper({children , styles ,onclick}) {
+const Wrapper = React.forwardRef(({ children, styles }, ref) => {
     return (
-        <div onClick={onclick} className={`${styles} bg-[#F7F7F7] flex flex-col items-center justify-center gap-5 rounded-xl`}>
+        <div ref={ref} className={`${styles} bg-[#F7F7F7] flex flex-col items-center justify-center gap-5 rounded-xl`}>
             {children}
         </div>
-    )
-}
+    );
+});
 
-export default Wrapper
+export default Wrapper;
+
