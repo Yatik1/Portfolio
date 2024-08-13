ceebe0d4aedab69655303546d5b17461f673fb33
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-13 14:27:03 +0530

    wrapper

--- components/ui/Wrapper.jsx
+++ components/ui/Wrapper.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Wrapper = React.forwardRef(({ children, styles }, ref) => {
+const Wrapper = React.forwardRef(({ children, styles  , key}, ref) => {
     return (
-        <div ref={ref} className={`${styles} bg-[#F7F7F7] flex flex-col items-center justify-center gap-5 rounded-xl`}>
+        <div key={key} ref={ref} className={`${styles} bg-[#F7F7F7] flex flex-col items-center justify-center gap-5 rounded-xl`}>
             {children}
         </div>
     );
