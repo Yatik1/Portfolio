0f3e001c1858d4eadb0085e99a7e03eee56c7362
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-16 23:03:21 +0530

    data

---
+++ data/projectData.jsx
@@ -0,0 +1,34 @@
+import store from "@/assets/storemockup.png";
+import dashboard from "@/assets/dashboardmockup.png";
+import cinemax from "@/assets/cinemaxmobilemockup.png"
+import chazily from "@/assets/chazilymockup.png"
+import blogbud from "@/assets/blogbudmockup.png"
+
+export const projectData = [
+    {
+      id:1,
+      src: dashboard,
+      title: "Dashboard & CMS",
+    },
+    {
+      id:2,
+      src: store,
+      title: "Store",
+    },
+    {
+      id:3,
+      src: blogbud ,
+      title : "Blog Bud"
+    } , 
+    {
+      id:4,
+      src:cinemax ,
+      title:"Cinemax"
+    } ,
+    {
+      id:5,
+      src:chazily,
+      title:"Chazily"
+    }
+  ];
+  
