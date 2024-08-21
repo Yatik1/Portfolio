bf6c0b4c0519797b2d40c68cb77bd7325065a07a
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-21 13:38:28 +0530

    bug

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,47 +1,55 @@
 "use client";
 
 import Wrapper from "@/components/ui/Wrapper";
-import { useGSAP } from "@gsap/react";
 import { motion } from "framer-motion";
 import gsap from "gsap";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 
 function HeroSection() {
 
     const buttonRef = useRef(null)
+    // const router = useRouter()
+    const isMobile = useRef(false)
 
     let content = "Driven by the art of crafting elegant and intuitive user interfaces, transforming them into visually compelling web experiences that are both functionally robust & aesthetically pleasing.";
     const words = content.split(" ");
 
-    const isMobile = window.innerWidth <= 768
 
-    if(!isMobile) {
-        useGSAP(() => {
+    useEffect(() => {
 
-            if(buttonRef.current) {
+        if (typeof window !== "undefined") {
+            isMobile.current = window.innerWidth <= 768;
+
+            const handleResize = () => {
+                isMobile.current = window.innerWidth <= 768;
+            };
+
+            window.addEventListener('resize', handleResize);
+
+            if(!isMobile.current && buttonRef.current) {
     
-            function mouseEnter() {
-                gsap.to(buttonRef.current , {
-                    width: "6rem",
-                    duration:0.5,
-                    ease:"power4.out"
-                })
-            }
-    
-            function mouseLeave() {
-                gsap.to(buttonRef.current , {
-                    width: "2.5rem",
-                    duration:0.5,
-                    ease:"power4.out"
-                })
-            }
-    
-                buttonRef.current.addEventListener("mouseenter" , mouseEnter)
-                buttonRef.current.addEventListener("mouseleave" , mouseLeave)
-            }
-        } , [isMobile])
-    
-    }
+                function mouseEnter() {
+                    gsap.to(buttonRef.current , {
+                        width: "6rem",
+                        duration:0.67,
+                        ease:"power4.out"
+                    })
+                }
+        
+                function mouseLeave() {
+                    gsap.to(buttonRef.current , {
+                        width: "40px",
+                        duration:0.5,
+                        ease:"power4.out"
+                    })
+                }
+        
+                    buttonRef.current.addEventListener("mouseenter" , mouseEnter)
+                    buttonRef.current.addEventListener("mouseleave" , mouseLeave)
+                }
+        }
+    } , [])    
 
     return (
         <Wrapper styles="w-[90vw] md:w-[95vw] h-[90vw] md:h-[42vw]">
@@ -67,6 +75,7 @@ function HeroSection() {
         <button 
             className="w-10 h-10 rounded-full bg-black text-white p-3 flex items-center justify-center mt-2"
             ref={buttonRef}
+            // onClick={() => router.push("/about")}
         >
             👁‍🗨
         </button>
