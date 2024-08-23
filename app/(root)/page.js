f018c5a754f6ae79cdbbe7f88eab8f8dbc7472a9
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-23 21:06:52 +0530

    root page

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,13 +1,15 @@
+import ContactSection from "@/components/ContactSection";
 import HeroSection from "@/components/HeroSection";
 import ProjectSection from "@/components/ProjectSection";
 import Header from "@/components/ui/Header";
+import Wrapper from "@/components/ui/Wrapper";
 
 
 function App() {
 
   
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center mb-4">
       <HeroSection />
 
       <Header
@@ -16,6 +18,19 @@ function App() {
       />
 
       <ProjectSection />
+
+      <Header 
+        title="Contacts"
+        textColor="text-[#d4d3d3] hover:text-stone-400"
+      />
+
+      <div className="px-5 w-full">
+      <Wrapper styles="w-full p-10">
+        <div className="flex item-center justify-center gap-3 md:gap-5 bg-white p-3 rounded-xl border border-[#dddd]">
+          <ContactSection />
+        </div>
+      </Wrapper>
+      </div>
     </div>
   );
 }
