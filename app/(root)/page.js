839ece0ce658ff6a71f4fde74cf0101f8d680551
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-20 17:21:11 +0530

    feat: implement loading animation and session tracking in App component

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,38 +1,56 @@
-import ContactSection from "@/components/ContactSection";
-import HeroSection from "@/components/HeroSection";
-import ProjectSection from "@/components/ProjectSection";
-import Header from "@/components/ui/Header";
-import Wrapper from "@/components/ui/Wrapper";
+"use client"
 
+import { useEffect, useState } from "react";
+import ProjectSection from "../../components/ProjectSection"
+import Header from "../../components/ui/Header";
+import HeroSection from "../../components/HeroSection";
 
-function App() {
+export default function App() {
+  const [showContent, setShowContent] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [isFirstVisit, setIsFirstVisit] = useState(false);
 
-  
-  return (
-    <div className="flex flex-col items-center justify-center mb-4">
-      <HeroSection />
+  useEffect(() => {
+    const hasVisited = sessionStorage.getItem("hasVisited");
+    if (hasVisited) {
+      setShowContent(true);
+    } else {
+      setIsFirstVisit(true);
+      const interval = setInterval(() => {
+        setProgress((prev) => {
+          if (prev >= 100) {
+            clearInterval(interval);
+            setTimeout(() => {
+              setShowContent(true);
+              sessionStorage.setItem("hasVisited", "true");
+            }, 300);
+            return 100;
+          }
+          return Math.min(prev+2, 100);
+        });
+      }, 70);
+    }
+  }, []);
 
-      <Header
-        title="My Projects"
-        textColor="text-[#d4d3d3] hover:text-stone-400"
-      />
-
-      <ProjectSection />
-
-      <Header 
-        title="Contacts"
-        textColor="text-[#d4d3d3] hover:text-stone-400"
-      />
-
-      <div className="px-5 w-full">
-      <Wrapper styles="w-full p-10">
-        <div className="flex item-center justify-center gap-3 md:gap-5 bg-white p-3 rounded-xl border border-[#dddd]">
-          <ContactSection />
+  if (!showContent && isFirstVisit) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-black flex-col transition-opacity duration-1000 ease-in-out">
+        <p className="text-sm font-mono text-white mb-4 pixelFont animate-pulse">Loading..</p>
+        <div className="relative w-72 h-1 bg-gray-800 rounded-full overflow-hidden">
+          <div
+            className="absolute left-0 top-0 h-full bg-white transition-all duration-[400ms] ease-in-out"
+            style={{ width: `${progress}%` }}
+          />
         </div>
-      </Wrapper>
       </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center mb-4 bg-[#FCFDF6] animate-fadeInZoom">
+      <Header />
+      <HeroSection />
+      <ProjectSection />
     </div>
   );
 }
-
-export default App;
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
+
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/ui/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,15 +10,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header 
-          title="Yatik Srivastava"
-          textColor="text-[#d4d3d3] hover:text-stone-400 cursor-pointer"
-        />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
