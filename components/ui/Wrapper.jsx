f0038a9e137711ee80f982166d89f179c7297d8d
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-20 17:22:24 +0530

    fix: update background color in Wrapper component

--- components/ui/Wrapper.jsx
+++ components/ui/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Wrapper = React.forwardRef(({ children, styles  , key}, ref) => {
     return (
-        <div key={key} ref={ref} className={`${styles} bg-[#F7F7F7] flex flex-col items-center justify-center gap-5 rounded-xl`}>
+        <div key={key} ref={ref} className={`${styles} bg-[#F9FAF2] flex flex-col items-center justify-center gap-5 rounded-xl`}>
             {children}
         </div>
     );
