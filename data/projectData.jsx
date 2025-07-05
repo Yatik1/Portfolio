834ff85ddf226c6b5156c52ea548bba1e5925290
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-05 17:01:30 +0530

    feat: update projectData to include new project and clean up unused entries

--- data/projectData.jsx
+++ data/projectData.jsx
@@ -1,59 +1,45 @@
 import store from "../assets/store.svg";
 import cms from "../assets/cms.svg";
 import chazel from "../assets/chazel.svg"
+import blogbud from "../assets/blogbud.svg"
 import formers from "../assets/formers.svg"
+
 
 import linkedin from "../assets/linkedin.png"
 import github from "../assets/github.png"
 import twitter from "../assets/twitter.png"
 import mail from "../assets/mail.png"
+import zone from "../assets/zone.svg"
+
 
 export const projectData = [
+      {
+      src:zone,
+      title:"Zone",
+      deployedLink:"https://github.com/Yatik1/zone-ui",
+    },
     {
       src: formers,
-      title:"formers",
-      cardWidth:"282px",
-      cardHeight:"403px",
-      bg:"bg-gradient-to-b from-[#595858] via-[#09090B] to-[#111113]",
-      position:"top-10 right-0",
+      title:"Formers",
       deployedLink:"https://formers.vercel.app/",
       githubLink:""
     },
     {
+      src:store ,
+      title:"Store",
+      deployedLink:"https://store-orcin-iota.vercel.app/"
+    },
+    {
       src: cms,
-      title:"cms",
-      cardWidth:"285px",
-      cardHeight:"364px",
-      bg:"bg-gradient-to-b from-[#BCE1FB] to-[#020817]",
-      position:"top-10 right-0",
+      title:"Dashboard & CMS",
       deployedLink:"https://estore-admin-panel-orcin.vercel.app/"
     },
     {
-      title:"Zone",
-      cardWidth:"517px",
-      cardHeight:"299px",
-      bg:"bg-gradient-to-b from-[#D7CABE] to-[#847D77]",
-      deployedLink:"",
-    },
-    {
-      src: chazel ,
-      title:"chazel",
-      cardWidth:"222px",
-      cardHeight:"324px",
-      bg:"bg-gradient-to-b from-[#FFFFFF] to-[#585858]",
-      position:"bottom-0 left-[1rem]",
-      deployedLink:"",
-
-    } , 
-    {
-      src:store ,
-      title:"store",
-      cardWidth:"285px",
-      cardHeight:"364px",
-      bg:"bg-gradient-to-b from-[#CBCDD4] to-[#595A5B]",
-      position:"top-10 right-0",
-      deployedLink:"https://store-orcin-iota.vercel.app/"
-    } 
+      src:blogbud,
+      title:"Blogbud",
+      deployedLink:"https://blogbud.vercel.app/"
+    }
+    
   ];
 
 
